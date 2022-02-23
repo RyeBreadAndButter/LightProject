0865de004c0c0c204c0e711476311cf7ba8d7bc5
Ryan Horgan <70227937+RyeBreadAndButter@users.noreply.github.com>
2022-02-23 10:06:07 -0500

    Add files via upload

---
+++ light/light/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  light
+//
+//  Created by Ryan Horgan on 2/22/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var lightButton: UIButton!
+    
+    
+    var lightOn = true
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateUI()
+    }
+
+    fileprivate func updateUI() {
+        if lightOn{
+            view.backgroundColor = .white
+            //lightButton.setTitle("Off", for: .normal)
+        } else {
+            view.backgroundColor = .black
+            //lightButton.setTitle("On", for: .normal)
+        }
+    }
+    
+    @IBAction func buttonPressed(_ sender: Any){
+        lightOn.toggle()
+        updateUI()
+        
+    }
+    
+}
+
